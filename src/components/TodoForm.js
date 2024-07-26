daed084051ce8c2cb629bcfe918fcbdd97f2aa40
harshil <harshil.hsj@gmail.com>
2024-07-27 02:18:08 +0530

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+import React , {useState} from "react";
+export const TodoForm = ({addTodo}) =>{
+    const handleSubmit = e =>{
+     e.preventDefault();
+     console.log(value);
+     addTodo(value);
+     setValue("");
+    }
+    const [value,setValue] = useState("")
+    return(
+        <form className="TodoForm" onSubmit={handleSubmit}>
+         <input type="text" className="todo-input" value = {value} placeholder="What is the task today?" onChange={ (e) =>setValue(e.target.value)}/>
+         <button type="submit" className="todo-btn">Add Task</button>
+         </form>
+    )
+}
